b4cd9b1beeb594f976fc9e8757820728041dea40
rahem sorour <sorourrahem17@gmail.com>
2023-07-22 23:19:18 +0100

    assignement completed

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
-// Write your code here
+// random
+let random= Math.floor(Math.random()*10)+1 
+console.log(random)
+
+// multiply
+let num1=2 ,num2=31
+let multiply=num1*num2
+console.log(multiply)
+
+// mod
+let num3=17,num4=13
+let mod=num3%num4
+console.log(mod)
+
+// max
+let n = [20,3,9,19];
+let max = Math.max(...n);
+console.log(max);
+
+
+
